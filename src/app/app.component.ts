9fd89f4f7c2b66ea087156be6ab2ff0ee983c170
Vanessa Tejerina <vanetejerina314@gmail.com>
2023-11-14 21:52:22 -0400

    -For pookie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,20 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular17';
+
+  toggle = false;
+  toggle1 = false;
+
+  newList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  handleClick() {
+    this.toggle = !this.toggle;
+    setTimeout(() => {
+      this.toggle1 = !this.toggle1;
+    }, 2000);
+  }
 }
